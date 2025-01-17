f330f716c714029fe02e0e8fafecce2c9c4e1989
Marco Fernández Callejón <marco.fernandezcallejon@hotmail.com>
2025-01-17 09:43:38 +0100

    Add files via upload

---
+++ srcs/stack_init.c
@@ -0,0 +1,67 @@
+#include "push_swap.h"
+
+static long ft_atoi(const char *s)
+{
+    long    result;
+    int     sign;
+
+    result = 0;
+    sign = 1;
+    while (*s == ' ' || *s == '\t' || *s == '\n' || 
+            *s == '\r' || *s == '\f' || *s == '\v')
+        s++;
+    if (*s == '-' || *s == '+')
+    {
+        if (*s == '-')
+            sign = -1;
+        s++;
+    }
+    while (ft_isdigit(*s))
+        result = result * 10 + (*s++ - '0');
+    return (result * sign);
+}
+
+static void append_node (t_stack_node **stack, int n)
+{
+    t_stack_node    *node;
+    t_stack_node    *last_node;
+
+    if (!stack)
+        return ;
+    node = malloc(sizeof(t_stack_node));
+    if (!node)
+        return ;
+    node->next = NULL;
+    node->nbr = n;
+    if(!(*stack))
+    {
+        * stack = node;
+        node->prev = NULL;
+    }
+    else
+    {
+        last_node = find_last(*stack);
+        last_node->next = node;
+        node->prev = last_node;
+    }
+}
+
+void    init_stack_a(t_stack_node **a, char **argv)
+{
+    long    n;
+    int     i;
+
+    i = 0;
+    while(argv[i])
+    {
+        if (error_syntax(argv[1]))
+            free_errors(a);
+        n = ft_atoi(argv[1]);
+        if (n > INT_MAX || n < INT_MIN)
+            free_errors(a);
+        if (error_duplicate(*a, (int)n))
+            free_errors(a);
+        append_node(a, (int)n);
+        i++;
+    }
+}
